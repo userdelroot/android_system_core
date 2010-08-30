8f6ffe20a91f94b98ebf04f1e70f9ec71df31437
cvpcs <root@cvpcs.org>
2010-08-29 19:36:16 -0500

    added BOARD_HIJACK_EXECUTABLES

--- logwrapper/Android.mk
+++ logwrapper/Android.mk
@@ -2,6 +2,12 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= logwrapper.c
+
+ifeq ($(filter $(BOARD_HIJACK_EXECUTABLES),logwrapper),)
 LOCAL_MODULE := logwrapper
+else
+LOCAL_MODULE := logwrapper.bin
+fi
+
 LOCAL_STATIC_LIBRARIES := liblog
 include $(BUILD_EXECUTABLE)
